a164272002044f46ed65e2a3308d419d78ec46c8
fleventy-5 <zeramarveenlyngkhoi@gmail.com>
2021-03-17 21:48:36 +0530

    update

--- firstModel.py
+++ firstModel.py
@@ -1,34 +1,27 @@
 import pandas as pd
+import numpy as np
+from sklearn import  svm
+from sklearn.model_selection import cross_validate, train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error
+
 
 weight_height_data = pd.read_csv('/home/zera/Documents/fleventy/ml-test-practice/csvFiles/weight-height.csv')
+weight_height_data.dropna(inplace= True)
+data_weight = weight_height_data['Weight']
+data_height = weight_height_data['Height']
+
+x = np.array(data_weight)
+
+y = np.array(data_height)
+x = x.reshape(-1,1)
 
 
-weight_height_data = weight_height_data.dropna(axis=0)
-y = weight_height_data.Weight
-
-features = ['Height']
-x = weight_height_data[features]
+x_train, x_test, y_train , y_test = train_test_split(x, y , test_size = 0.3, random_state =1)
 
 
-y_test ,y_train = train_test_split(y,test_size = 0.33,random_state =1)
-
-x_test ,x_train = train_test_split(x,test_size = 0.33,random_state =1)
-
-model_1 =  LinearRegression()
+model_1 = svm.SVR()
 model_1.fit(x_train,y_train)
 
-YPredict = model_1.predict(x_test)
+confidence = model_1.score(x_test,y_test)
 
-model_accuracy = mean_absolute_error(y_test,YPredict)
-
-#print(y_test.shape,y_train.shape)
-#print(YPredict)
-
-
-print(model_accuracy)
-
-
-#print(model_accuracy/len(y))
+print(confidence)
