8ed9ce2c2a43c09db6f7753d5acc8aab898d6a9e
fleventy-5 <zeramarveenlyngkhoi@gmail.com>
2021-03-17 22:12:35 +0530

    Update

--- firstModel.py
+++ firstModel.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn import  svm
-from sklearn.model_selection import cross_validate, train_test_split
+from sklearn.model_selection import  train_test_split
 from sklearn.linear_model import LinearRegression
 
 
@@ -11,16 +10,20 @@ data_weight = weight_height_data['Weight']
 data_height = weight_height_data['Height']
 
 x = np.array(data_weight)
-y = np.array(data_height)
 x = x.reshape(-1,1)
+y = np.array(data_height)
+y = y.reshape(-1,1)
 
 
-x_train, x_test, y_train , y_test = train_test_split(x, y , test_size = 0.33, random_state =1)
+x_train, x_test, y_train , y_test = train_test_split(x, y , test_size = 0.044, random_state =1)
+
 
 
 model_1 = LinearRegression()
 model_1.fit(x_train,y_train)
 
-confidence = model_1.score(x_test,y_test)
+Y_predict = model_1.predict(x)
 
+confidence = model_1.score(x_test,y_test)
 print(confidence)
+print(Y_predict)
